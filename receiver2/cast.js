33cd29bc8d80030ceb18cd46db28426577408fcd
Florian Fesseler <ffesseler@kapit.fr>
2014-05-14 21:07:44 +0200

    lazy

--- receiver2/cast.js
+++ receiver2/cast.js
@@ -1,76 +1,74 @@
-window.debug = false;
+window.debug = true;
 
 function checkDebug() {
-    if (window.debug) {
-        document.getElementById('debug').style.display = 'block';
-        document.getElementById('maze').style.margin = '30px';
-    } else {
-        document.getElementById('debug').style.display = 'none';
-        document.getElementById('maze').style.margin = '30px auto';
-    }
+  if (window.debug) {
+    document.getElementById('debug').style.display = 'block';
+    document.getElementById('maze').style.margin = '30px';
+  } else {
+    document.getElementById('debug').style.display = 'none';
+    document.getElementById('maze').style.margin = '30px auto';
+  }
 }
 
 $(document).ready(function () {
 
-    cast.receiver.logger.setLevelValue(0);
+  cast.receiver.logger.setLevelValue(cast.receiver.LoggerLevel.NONE);
 
-    window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
-    window.castReceiverManager.start();
-    log('Starting receiver manager');
+  window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
+  window.castReceiverManager.start();
+  log('Starting receiver manager');
 
-    window.castReceiverManager.onReady = function (event) {
-        log('Received ready event ' + JSON.stringify(event.data));
-        window.castReceiverManager.setApplicationState("Application status is ready...");
-    };
+  window.castReceiverManager.onReady = function (event) {
+    log('Received ready event ' + JSON.stringify(event.data));
+    window.castReceiverManager.setApplicationState("Application status is ready...");
+  };
 
-    window.messageBus = window.castReceiverManager.getCastMessageBus('urn:x-cast:fr.xebia.workshop.cast.maze');
+  window.messageBus = window.castReceiverManager.getCastMessageBus(namespace);
 
-    /**
-     * When sender connected
-     */
-    window.castReceiverManager.onSenderConnected = function (event) {
-        log('Received sender connected event ' + event.data);
-        var color = addPlayer(event.senderId);
-        window.messageBus.send(event.senderId, JSON.stringify({color: color}));
-    };
+  /**
+   * When sender connected
+   */
+  window.castReceiverManager.onSenderConnected = function (event) {
+    addPlayer(event.senderId);
+  };
 
-    /**
-     * When sender disconnect
-     * @param event
-     */
-    window.castReceiverManager.onSenderDisconnected = function (event) {
-        log('Received sender disconnected event ' + event.data);
-        removePlayer(event.senderId);
-        if (window.castReceiverManager.getSenders().length == 0) {
-            window.close();
-        }
-    };
-
-    /**
-     * When message received
-     * @param event object
-     */
-    window.messageBus.onMessage = function (event) {
-        log('Message [' + event.senderId + '] ' + event.data);
-        handleKeypress(event.data, event.senderId);
-    };
-
-    /**
-     * Start receiver
-     */
-    window.castReceiverManager.start({statusText: "Application is starting"});
-    log('Receiver manager started');
-
-    /**
-     * Misc method to log into console box in web view
-     * @param msg message to log
-     */
-    function log(msg) {
-        if (window.debug) {
-            var debug = document.getElementById('debug');
-            var tmpHTML = debug.innerHTML;
-            debug.innerHTML = '';
-            debug.innerHTML = msg + '<br/>' + tmpHTML;
-        }
+  /**
+   * When sender disconnect
+   * @param event
+   */
+  window.castReceiverManager.onSenderDisconnected = function (event) {
+    log('Received sender disconnected event ' + event.data);
+    removePlayer(event.senderId);
+    if (window.castReceiverManager.getSenders().length == 0) {
+      window.close();
     }
+  };
+
+  /**
+   * When message received
+   * @param event object
+   */
+  window.messageBus.onMessage = function (event) {
+    log('Message [' + event.senderId + '] ' + event.data);
+    handleKeypress(event.data, event.senderId);
+  };
+
+  /**
+   * Start receiver
+   */
+    // TODO start CastReceiverManager
+  log('Receiver manager started');
+
+  /**
+   * Misc method to log into console box in web view
+   * @param msg message to log
+   */
+  function log(msg) {
+    if (window.debug) {
+      var debug = document.getElementById('debug');
+      var tmpHTML = debug.innerHTML;
+      debug.innerHTML = '';
+      debug.innerHTML = msg + '<br/>' + tmpHTML;
+    }
+  }
 });
